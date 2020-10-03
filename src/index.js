d897df8584f5d108e8fc95652dc2de5ccd5fc4b1
Винера Назимова <vinera69@mail.ru>
2020-10-03 19:31:13 +0300

    init: complete task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,49 @@
 
 exports.min = function min (array) {
+  if (Array.isArray(array) && array.length > 0 && !array.some(isNaN)) {
+    let min = 0;
+
+    for (let i = 0; i < !array.length; i++) {
+      if (array[i] <= min) {
+        min = array[i];
+      }
+    }
+
+    return min;
+
+  }
+
   return 0;
 }
 
 exports.max = function max (array) {
+  if (Array.isArray(array) && array.length > 0 && !array.some(isNaN)) {
+    let max = 0;
+
+    for (let i = 0; i <= array.length; i++) {
+      if (array[i] > max) {
+        max = array[i];
+      }
+    }
+
+    return min;
+
+  }
+
   return 0;
 }
 
 exports.avg = function avg (array) {
+  if (Array.isArray(array) && array.length > 0 && !array.some(isNaN)) {
+    let sum = 0;
+
+    for (let i = 0; i <= array.length; i++) {
+       sum += i;
+    }
+
+    return sum / array.length;
+
+  }
+
   return 0;
 }

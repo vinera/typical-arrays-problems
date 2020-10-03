d0bf4d771d1e390cc215383ec7f0f1f7a2b64e47
Винера Назимова <vinera69@mail.ru>
2020-10-03 19:51:16 +0300

    fix: bugs fixed

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ exports.min = function min (array) {
   if (Array.isArray(array) && array.length > 0 && !array.some(isNaN)) {
     let min = 0;
 
-    for (let i = 0; i < !array.length; i++) {
-      if (array[i] <= min) {
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] < min) {
         min = array[i];
       }
     }
@@ -20,13 +20,13 @@ exports.max = function max (array) {
   if (Array.isArray(array) && array.length > 0 && !array.some(isNaN)) {
     let max = 0;
 
-    for (let i = 0; i <= array.length; i++) {
+    for (let i = 0; i < array.length; i++) {
       if (array[i] > max) {
         max = array[i];
       }
     }
 
-    return min;
+    return max;
 
   }
 
@@ -37,8 +37,8 @@ exports.avg = function avg (array) {
   if (Array.isArray(array) && array.length > 0 && !array.some(isNaN)) {
     let sum = 0;
 
-    for (let i = 0; i <= array.length; i++) {
-       sum += i;
+    for (let i = 0; i < array.length; i++) {
+       sum += array[i];
     }
 
     return sum / array.length;
